0964c62df727cf907d8878b3db651b65e4748d56
edguzgar <42489302+edguzgar@users.noreply.github.com>
2019-12-19 04:25:40 +0100

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -10,12 +10,10 @@ unsigned int *fibonacci(size_t n)
     } else if (n == 1) {
         arr = (unsigned int*)malloc(n*sizeof(unsigned int));
         arr[0] = 0;
-        return arr;
     } else if (n == 2) {
         arr = (unsigned int*)malloc(n*sizeof(unsigned int));
         arr[0] = 0;
         arr[1] = 1;
-        return arr;
     } else {
         arr = (unsigned int*)malloc(n*sizeof(unsigned int));
         arr[0] = 0;
@@ -24,8 +22,7 @@ unsigned int *fibonacci(size_t n)
         size_t i;
         for(i = 2; i < n; ++i) {            // Array indexing seems more idiomatic
             arr[i] = arr[i-1] + arr[i-2];
-        }      
-        
-        return arr;
+        }
     }
+    return arr;
 }
