08f7e18f394a6e4303f1a7a26a0c0e3260ba1e1e
edguzgar <eduguzgar@gmail.com>
2021-08-16 16:17:02 +0200

    added loop unrolling by factor K=2

--- fibonacci.c
+++ fibonacci.c
@@ -8,17 +8,20 @@ void fibonacci(unsigned int arr[], size_t n)
         return;
     } else if (n == 1) {
         arr[0] = 0;
-    } else if (n == 2) {
-        arr[0] = 0;
-        arr[1] = 1;
-    } else {
-        arr[0] = 0;
-        arr[1] = 1;
+        return;
+    }
 
-        size_t i;
-        for(i = 2; i < n; ++i) {            // Array indexing seems more idiomatic
-            arr[i] = arr[i-1] + arr[i-2];
-        }
+    arr[0] = 0;
+    arr[1] = 1;
+
+    size_t i;
+    for (i = 2; i < n % 2 + 2; ++i) {   // K = 2
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    for (; i < n; i+=2) {               // K = 2
+        arr[i] = arr[i-1] + arr[i-2];
+        arr[i+1] = arr[i] + arr[i-1];
     }
 }
 
@@ -32,9 +35,9 @@ int main(void)
     
     if(n == 0) 
     {
-	putchar('\n');
-	fflush(stdin);
-	puts("Press ENTER to continue ... ");
+    putchar('\n');
+    fflush(stdin);
+    puts("Press ENTER to continue ... ");
         getchar();
         return EXIT_SUCCESS;
     }
@@ -61,7 +64,7 @@ int main(void)
     printf("\n\n");
     
     fflush(stdin);
-	
+    
     puts("Press ENTER to continue ... ");
     getchar();
     
